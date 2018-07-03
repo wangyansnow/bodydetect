07d9189d4841591d5a3be6ee3fd419331d074b41
wangyansnow <wangyan6@cmcm.com>
2018-07-03 16:56:00 +0800

    Initial Commit

---
+++ wyliehu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wyliehu
+//
+//  Created by 王俨 on 2018/7/3.
+//  Copyright © 2018年 https://github.com/wangyansnow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wyliehu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wyliehu
+//
+//  Created by 王俨 on 2018/7/3.
+//  Copyright © 2018年 https://github.com/wangyansnow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
